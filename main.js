91a5873a39662f17ac3a7c194ad479331a8b6a2c
giosquisate <160138963+giosquisate@users.noreply.github.com>
2025-04-25 09:48:17 -0300

    Ana Strapazzon Giovanna Guilherme

---
+++ main.js
@@ -0,0 +1,10 @@
+let randomNumber =
+Math.floor(Math.random() * 10) +1;
+let attempts = 0;
+
+function checkGuess (){
+    const userGuess = parseInt(document.getElementById('guess').value);
+    attempts++;
+
+    if(userGuess === randomNUmber)
+}
